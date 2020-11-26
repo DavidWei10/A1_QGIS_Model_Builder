2b02b05a4bd97b9e2fcda87af31c7a6a6db9a0bf
DavidWei10 <73949391+DavidWei10@users.noreply.github.com>
2020-11-26 01:46:02 +0100

    Dateien verbessert

--- calculate_slope.bat
+++ calculate_slope.bat
@@ -1,7 +1,7 @@
 ogr2ogr -f "ESRI Shapefile" -sql "SELECT * from gadm36_SVN_2 where NAME_2 = 'Izola'" izola.shp gadm36_SVN.gpkg
 
 
-Gdalwarp -t_srs EPSG:32632 -cutline izola.shp dem_merge.tif dem_merge_neu_izola.tif
+Gdalwarp -t_srs EPSG:32632 -cutline izola.shp -crop_to_cutline dem_merge.tif dem_merge_neu_izola.tif
 
 gdaldem slope dem_merge_neu_izola.tif dem_merge_neu_slope_izola.tif
 
--- calculate_slope_hillshade.bat
+++ calculate_slope_hillshade.bat
@@ -1,6 +1,6 @@
 ogr2ogr -f "ESRI Shapefile" -sql "SELECT * from gadm36_SVN_2 where NAME_2 = 'Koper'" koper.shp gadm36_SVN.gpkg
 
-Gdalwarp -t_srs EPSG:32632 -cutline koper.shp dem_merge.tif dem_merge_neu_koper.tif
+Gdalwarp -t_srs EPSG:32632 -cutline koper.shp -crop_to_cutline dem_merge.tif dem_merge_neu_koper.tif
 
 gdaldem slope dem_merge_neu_koper.tif dem_merge_neu_slope_koper.tif
 
